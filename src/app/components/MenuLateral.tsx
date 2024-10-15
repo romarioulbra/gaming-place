d7fc7ae112bcfdb424b2821f1ab922ebfcfc5323
Romario Dias Paz <romariodp07@gmail.com>
2024-10-14 23:51:57 -0300

    Update MenuLateral.tsx
    Ajuste em Menu Lateral - Map

--- src/app/components/MenuLateral.tsx
+++ src/app/components/MenuLateral.tsx
@@ -12,33 +12,36 @@ export default function Sidebar() {
     setIsOpen(!isOpen);
   };
 
+  const listaData = [
+    {
+      title: "Configurações Gerais",
+      link: "/config/geral"
+    },
+    {
+      title: "Usuários",
+      link: "/config/perfil"
+    },
+   {
+      title: "Jogos",
+      link: "/config/notificacoes"
+    },
+    {
+      title: "Categorias de Jogos",
+      link: "/config/seguranca"
+    }
+  ];
+
   return (
     <>
       {/* Menu Lateral */}
       <aside className={`fixed top-0 left-0 w-64 bg-violet-800 text-white p-4 h-full transition-transform transform ${isOpen ? "translate-x-0" : "-translate-x-full"} sm:translate-x-0 sm:relative z-20`}>
           <h3 className="text-2xl font-bold mb-6 mt-24 flex"> <FaGaugeHigh className='mr-1'/> Configurações</h3>
-          <ul className="space-y-4">
-            <li>
-              <Link href="/config/geral" className="block text-lg hover:text-blue-400 ">
-                Configurações Gerais
-              </Link>
-            </li>
-            <li>
-              <Link href="/config/perfil" className="block text-lg hover:text-blue-400">
-                Perfil
-              </Link>
-            </li>
-            <li>
-              <Link href="/config/seguranca" className="block text-lg hover:text-blue-400">
-                Jogos
-              </Link>
-            </li>
-            <li>
-              <Link href="/config/notificacoes" className="block text-lg hover:text-blue-400">
-                Categorias de Jogos
-              </Link>
-            </li>
-          </ul>
+
+          {listaData.map((lista, index) => (
+            <Link key={index} href={lista.link} className="block text-lg hover:text-blue-400 ">
+                <div className="space-y-4 mb-3">{ lista.title }</div> 
+            </Link>
+          ))}
       </aside>
 
       {/* Botão de Menu para dispositivos móveis */}

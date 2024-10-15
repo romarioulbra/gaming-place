475b4f2ff9f2366a1cd65724fb6a789aecade7e0
Romario Dias Paz <romariodp07@gmail.com>
2024-10-14 23:41:37 -0300

    Update MenuLateral.tsx
    Ajuste no Menu Lateral

--- src/app/components/MenuLateral.tsx
+++ src/app/components/MenuLateral.tsx
@@ -16,30 +16,29 @@ export default function Sidebar() {
     <>
       {/* Menu Lateral */}
       <aside className={`fixed top-0 left-0 w-64 bg-violet-800 text-white p-4 h-full transition-transform transform ${isOpen ? "translate-x-0" : "-translate-x-full"} sm:translate-x-0 sm:relative z-20`}>
-      
-      <h3 className="text-2xl font-bold mb-6 mt-8 flex"> <FaGaugeHigh className='mr-1'/> Configurações</h3>
-        <ul className="space-y-4">
-          <li>
-            <Link href="/config/geral" className="block text-lg hover:text-blue-400 ">
-              Configurações Gerais
-            </Link>
-          </li>
-          <li>
-            <Link href="/config/perfil" className="block text-lg hover:text-blue-400">
-              Perfil
-            </Link>
-          </li>
-          <li>
-            <Link href="/config/seguranca" className="block text-lg hover:text-blue-400">
-              Segurança
-            </Link>
-          </li>
-          <li>
-            <Link href="/config/notificacoes" className="block text-lg hover:text-blue-400">
-              Notificações
-            </Link>
-          </li>
-        </ul>
+          <h3 className="text-2xl font-bold mb-6 mt-24 flex"> <FaGaugeHigh className='mr-1'/> Configurações</h3>
+          <ul className="space-y-4">
+            <li>
+              <Link href="/config/geral" className="block text-lg hover:text-blue-400 ">
+                Configurações Gerais
+              </Link>
+            </li>
+            <li>
+              <Link href="/config/perfil" className="block text-lg hover:text-blue-400">
+                Perfil
+              </Link>
+            </li>
+            <li>
+              <Link href="/config/seguranca" className="block text-lg hover:text-blue-400">
+                Jogos
+              </Link>
+            </li>
+            <li>
+              <Link href="/config/notificacoes" className="block text-lg hover:text-blue-400">
+                Categorias de Jogos
+              </Link>
+            </li>
+          </ul>
       </aside>
 
       {/* Botão de Menu para dispositivos móveis */}

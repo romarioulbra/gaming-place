57c6b2b11dd0ede3885ee4363e2eb9e8aa7f0785
Romario Dias Paz <romariodp07@gmail.com>
2024-10-17 01:16:33 -0300

    Componente - Botão

---
+++ src/app/components/Botao.tsx
@@ -0,0 +1,27 @@
+'use client'
+interface BotaoProps {
+  texto: string;
+  tipo?: 'button' | 'submit'; // Tipo do botão, opcional, padrão será 'submit'
+  onClick?: () => void; 
+  cor?: 'verde' | 'azul' | 'amarelo' | 'vermelho'; // Parâmetro de cor      // Função opcional que será chamada quando clicado, se for 'button'
+}
+
+export default function Botao({ texto, cor, tipo = 'submit', onClick }: BotaoProps) {
+  
+      const coresClasses = {
+        verde: 'bg-green-500 hover:bg-green-600',
+        azul: 'bg-blue-500 hover:bg-blue-600',
+        amarelo: 'bg-yellow-500 hover:bg-yellow-600 text-black', // Amarelo fica melhor com texto preto
+        vermelho: 'bg-red-500 hover:bg-red-600',
+      };
+
+  return (
+    <button
+      type={tipo}
+      className={`${coresClasses[cor]} w-full text-white p-2 rounded-lg font-bold hover:bg-blue-700 transition-colors duration-300`}
+      onClick={onClick} // O onClick será executado se passado
+    >
+      {texto}
+    </button>
+  );
+}

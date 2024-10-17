87f59e3e73664a40f425299e5d49571735c3b8f5
Romario Dias Paz <romariodp07@gmail.com>
2024-10-17 01:19:11 -0300

    Módulo Categoria Jogos - Viwer e Form

---
+++ src/app/configuracao/jogosCategoria/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import Navbar from "@/app/components/MenuNavbar";
+import Footer from "@/app/components/Rodape";
+import CadastroCatJogos from "./cadastro";
+
+export default function JogosCategoria() {
+
+  return (
+    <>
+      <Navbar/>
+      <div className="flex flex-col items-center justify-center h-screen">
+        {/* Título */}
+        <h2 className="text-2xl font-bold mb-4">Cadastro de Categorias de Jogos</h2>
+
+        {/* Formulário de Cadastro */}
+        <CadastroCatJogos />
+      </div>
+      <Footer />
+    </>
+  );
+}

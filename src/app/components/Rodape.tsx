70eec558fe017dfe38f9af40b8d1ddc301aab95a
Romario Dias Paz <romariodp07@gmail.com>
2024-10-25 12:42:31 -0300

    Alterações na responsividade das logos

--- src/app/components/Rodape.tsx
+++ src/app/components/Rodape.tsx
@@ -3,29 +3,64 @@ import Image from 'next/image'; // Usado para carregar a imagem no Next.js
 import { GrGamepad } from "react-icons/gr";
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center justify-between p-4 bg-gray-800 text-white border-t-2 border-white">
-      {/* Logo Alinhada à Esquerda */}
-      <div className="flex items-center justify-between w-full max-w-6xl">
-        <div className="flex items-center">
-          <Image src="/img/ulbraPalmas-light.svg" alt="Logo" width={110} height={110} /> 
-        </div>
-        {/* Texto Centralizado */}    
-        <div className="flex text-white text-xl font-bold space-x-2 ">
-            <GrGamepad className='w-8 h-8'/>
-            <h2>  Gaming Place</h2><br />
-        </div>
-            {/* <strong className="text-sm">Direitos Reservados &copy; {new Date().getFullYear()}</strong> */}
-        {/* Botões Alinhados à Direita */}
-        <div className="flex flex-col items-end">
-          <div className="flex space-x-2">
-            <Image src="/img/logoCienciasH.png" alt="logo" width={110} height={110} /> 
-            <Image src="/img/logoEngenhariaH.png" alt="logo" width={110} height={110} />
+      <footer className="flex flex-col items-center justify-between p-4 bg-gray-900 text-gray-200 py-4 border-t-2 border-white">
+      {/* /  <footer className="flex flex-col items-center justify-between p-4 bg-fuchsia-500 text-gray-200 py-4 border-t-2 border-white"> */}
+        {/* Logo Alinhada à Esquerda */}
+        <div className="flex items-center justify-between w-full max-w-6xl space-x-4">
+          <div className="flex items-center">
+            <Image src="/img/ulbraPalmas-light.svg" 
+              alt="Logo" 
+              width={90} 
+              height={90} 
+              className="w-30 h-30 md:w-28 md:h-28"  // Ajuste para telas maiores 
+            /> 
           </div>
-          {/* <div className="flex space-x-2 mt-2">
-            <Image src="/img/logoEngenhariaH.png" alt="logo" width={90} height={90} /> 
+          {/* Texto Centralizado */}    
+          <div className="flex text-white text-xl font-bold space-x-2 ">
+              <GrGamepad className="w-6 h-6 md:w-5 md:h-5" />
+              <h2 className=" max-w-xs break-words" >Gaming Place</h2>
+          </div>
+          
+          {/* <strong className="text-sm">Direitos Reservados &copy; {new Date().getFullYear()}</strong> */}
+          {/* Botões Alinhados à Direita */}
+           <div className="flex flex-col items-end md:items-end mr-5">
+              <div className="flex space-x-2">           
+                <Image src="/img/logoCienciasH.png" 
+                  alt="logo" 
+                  width={110} 
+                  height={110} 
+                /> 
+                <Image src="/img/logoEngenhariaH.png" 
+                  alt="logo" 
+                  width={110} 
+                  height={110}
+                />
+              </div>
+          </div>
+
+        {/*
+          <div className="flex flex-col items-center md:items-end mr-5">
+            <div className="flex space-y-2">           
+              <Image 
+                src="/img/logoCiencias.png" 
+                alt="logo" 
+                width={60} 
+                height={60} 
+                className='mr-1 pb-2 pr-2 pl-2 pt-4'
+              /> 
+              <Image 
+                src="/img/logoEngenharia.png" 
+                alt="logo" 
+                width={60} 
+                height={100}
+                className='object-contain  pb-2 pr-2 pl-2 pt-2'
+              />
+            </div>
           </div> */}
+
+          
+
         </div>
-      </div>
-    </footer>
+      </footer>
   );
 }

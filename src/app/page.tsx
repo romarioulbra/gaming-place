0b587972aac31d7829f8a0855b4dea7c92ddad89
Romario Dias Paz <romariodp07@gmail.com>
2024-10-25 12:41:02 -0300

    Alterações no Layout - Tela de Boas Vindas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,17 @@ import Navbar from './components/MenuNavbar';
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen bg-gradient-to-r from-purple-700  to-fuchsia-500/75">
+      <div className="min-h-screen bg-gradient-to-r from-purple-700 to-purple-300">
         <Navbar/>
         {/* <MenuHamburguer/> */}
         {/* <NavbarActive/> */}
-          <header className="flex flex-col items-center justify-center min-h-screen">
+          <header className="flex flex-col items-center justify-center h-screen space-y-6">
             <div className="text-center">
-              <h1 className="text-3xl font-bold ml-6 text-purple-900">Seja Bem-vindo ao Gaming Place!</h1>
-              <p className="text-lg text-fuchsia-900">Explore os incríveis jogos desenvolvidos no CeulpUlbra</p>
+              <h1 className="text-4xl font-bold text-white">Seja Bem-vindo ao Gaming Place!</h1>
+              <p className="text-lg text-gray-200">Explore os incríveis jogos desenvolvidos no CeulpUlbra</p>
             </div>
-              <div className="ml-4 flex justify-end mt-5">
-                <SlGameController className="w-60 h-60 text-white" />
+              <div className="ml-4 flex justify-end mt-8">
+                <SlGameController className="w-40 h-40 text-white hover:scale-110 transition-transform duration-300" />
               </div>
           </header>
           <Footer></Footer>

39d11c067b802cb5e10c44148b3e80b5aa62f91c
Romario Dias Paz <romariodp07@gmail.com>
2024-10-30 13:49:26 -0300

    Efeito Pespectiva em Card

--- src/app/components/CardJogo.tsx
+++ src/app/components/CardJogo.tsx
@@ -2,7 +2,8 @@ import Image from 'next/image';
 export default function CardJogo({ title, image, buttonText }) {
   return (
      <>
-      <div className="bg-purple-600 rounded-lg p-6 ml-16 grid justify-items-center shadow-lg shadow-slate-300 rotate-2 hover:scale-105 transition-transform duration-300">
+      <div className="bg-purple-600 rounded-lg p-6 ml-16 grid justify-items-center shadow-lg shadow-slate-300 rotate-2 hover:scale-105 transition-transform duration-300 -skew-y-6
+">
           <Image src={image} alt={title} width={300} height={200} />
           <h2 className="text-2xl font-bold text-white text-center mt-2 mb-2">{title}</h2>
           <button className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded w-60">{buttonText}</button>
--- src/app/components/Imagens.tsx
+++ src/app/components/Imagens.tsx
@@ -4,8 +4,9 @@ export default function ImagensCirculares({caminho}) {
   return (
     <>
       {/* <div className="flex justify-center gap-4 mt-8"> */}
-      <div className="">
-          <Image src={caminho} alt="Imagem 1" width={200} height={200} className="w-24 h-24 rounded-full object-cover border-white border-2 p-2 shadow-lg shadow-fuchsia-500"/>
+      <div className=""> 
+      {/* animate-bounce */}
+          <Image src={caminho} alt="Imagem 1" width={200} height={200} className="w-24 h-24 rounded-full object-cover border-white border-2 p-2 shadow-lg shadow-fuchsia-500 "/>
       </div> 
       
     </>

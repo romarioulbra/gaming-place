ebc00969b70ac2e23034f5bb92139f47d1c47350
Romario Dias Paz <romariodp07@gmail.com>
2024-10-29 23:58:52 -0300

    Viwer - Ajustes Gerais em Componentes

--- src/app/configuracao/usuarios/page.tsx
+++ src/app/configuracao/usuarios/page.tsx
@@ -1,37 +1,49 @@
 'use client'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 import Navbar from "@/app/components/MenuNavbar";
 import Footer from "@/app/components/Rodape";
 import CadastroUsuario from "./cadastro";
-import TabelaDark from "@/app/components/TabelaDark";
-import { useEffect, useState } from "react";
-
+import CarregarTela from "@/app/components/Loading";
+import Tabela from "@/app/components/Tabela";
+// import TabelaDark from "@/app/components/TabelaDark";
+// import TabelaDinamica from "@/app/components/TabelaDinamica";
 
 
 export default function Usuarios() {
 
-  const data = [
-    { id: 1, usuario_nome: 'João Silva', usuario_email: 'joao@example.com',usuario_senha: '123456', usuario_nivel:'Admin' },
-    { id: 2, usuario_nome: 'Rom', usuario_email: 'joao@example.com',usuario_senha: '123456', usuario_nivel:'Comum' },
-  ];
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await axios.get('/api/usuarios');
+        setUsuarios(response.data);
+        console.log(usuarios); 
+      } catch (error) {
+        console.error('Erro ao buscar usuários:', error);
+      }
+    }
+    fetchUsers();
+  }, []);
 
   return (
     <>
       <Navbar/>
       <div className="flex flex-col items-center justify-center h-screen">
-        {/* Título */}
-        <h2 className="text-2xl font-bold mb-4">Cadastro de Usuários</h2>
-        {/* Formulário de Cadastro */}
-        <CadastroUsuario />
+          <h2 className="text-2xl font-bold mb-4">Cadastro de Usuários</h2>
+          <CadastroUsuario />
       </div>
 
       <div className="flex flex-col items-center justify-center h-screen">
-        {/* Título */}
         <h2 className="text-2xl font-bold mb-4">Listagem de Usuários</h2>
-        <TabelaDark data={data}/>
-      
+        {/* <TabelaDark data={usuarios}/><br /> */}
+        {/* <TabelaDinamica initialUsers={usuarios} /> */}
+        <Tabela data={usuarios}/>
+        <CarregarTela/>
       </div>
-      <Footer />
+      <Footer/>
     </>
   );
 }

7178a520a2638beee1d7b816c738a9ed7c3c4f75
Romario Dias Paz <romariodp07@gmail.com>
2024-11-05 13:47:11 -0300

    Formulário - Cadastro Usuário

--- src/app/configuracao/usuarios/page.tsx
+++ src/app/configuracao/usuarios/page.tsx
@@ -5,13 +5,18 @@ import { useEffect, useState } from "react";
 import Navbar from "@/app/components/MenuNavbar";
 import Footer from "@/app/components/Rodape";
 import CadastroUsuario from "./cadastro";
-import CarregarTela from "@/app/components/Loading";
+import CarregarTela from "@/app/components/Loading2";
 import Tabela from "@/app/components/Tabela";
+import { FaUser} from "react-icons/fa";
 // import TabelaDark from "@/app/components/TabelaDark";
 // import TabelaDinamica from "@/app/components/TabelaDinamica";
-
+import CabecalhoViwer from "../../components/CabecalhoViwer";
+import Fly from "@/app/components/Flyout";
 
 export default function Usuarios() {
+    
+  // variáveis globais
+  const nomeModulo = 'Perfis de Usuários';
 
   const [usuarios, setUsuarios] = useState([]);
 
@@ -31,17 +36,25 @@ export default function Usuarios() {
   return (
     <>
       <Navbar/>
-      <div className="flex flex-col items-center justify-center h-screen">
+      <Fly/>
+      
+      <CabecalhoViwer 
+        nomeModel={nomeModulo} 
+        Icone={FaUser}
+      />
+
+      {/* <div className="flex flex-col items-center justify-center h-screen">
           <h2 className="text-2xl font-bold mb-4">Cadastro de Usuários</h2>
           <CadastroUsuario />
-      </div>
+      </div> */}
 
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h2 className="text-2xl font-bold mb-4">Listagem de Usuários</h2>
+      <div className="">
+      {/* <div className="flex flex-col items-center justify-center h-screen"> */}
+        {/* <h2 className="text-2xl font-bold mb-4">Listagem de Usuários</h2> */}
         {/* <TabelaDark data={usuarios}/><br /> */}
         {/* <TabelaDinamica initialUsers={usuarios} /> */}
         <Tabela data={usuarios}/>
-        <CarregarTela/>
+        {/* <CarregarTela/> */}
       </div>
       <Footer/>
     </>

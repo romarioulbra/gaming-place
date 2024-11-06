4cb558bc67c286e0507494b844dd4592ae210c41
Romario Dias Paz <romariodp07@gmail.com>
2024-11-06 01:32:08 -0300

    limpeza de Código

--- src/app/configuracao/page.tsx
+++ src/app/configuracao/page.tsx
@@ -1,35 +1,22 @@
 'use client'
-import Sidebar from '../components/MenuLateral';
+// import Sidebar from '../components/MenuLateral';
 import CardsGrid from '../components/CardsPainel';
-import Navbar from '../components/MenuNavbar';
-import Footer from "../components/Rodape";
 
 export default function ConfigPanel() {
 
-  // const config ="Configurações Gerais";
-  // const usuarios = "usuarios";
-  // const catJogos = "Categoria de Jogos";
-  // const jogos = "Jogos";
-
-
   return (
     <>
-      <Navbar/>
       <div className="flex h-screen mt-9">
           {/* Menu Lateral */}
-          <Sidebar />
-
+          {/* <Sidebar /> */}
           {/* Painel Principal */}
-          <main className="flex-1 bg-gray-100 p-6 ml-64 sm:ml-0">
+          <div className="flex-1 bg-gray-100 p-6  sm:ml-0">
             <h1 className='text-center  font-bold text-2xl mt-5 mb-5 text-purple-700'>Painel de Configuração</h1>
             <CardsGrid 
             />
-          </main>
+          </div>
       </div>
-   
-      {/* <div className="min-h-screen bg-gradient-to-tr from-purple-500 to-purple-300 flex h-screen mt-9">    
-      </div>   */}
-    <Footer/>
+  
     </>
   );
 }
--- src/app/configuracao/usuarios/page.tsx
+++ src/app/configuracao/usuarios/page.tsx
@@ -4,12 +4,8 @@ import { useEffect, useState } from "react";
 
 import Navbar from "@/app/components/MenuNavbar";
 import Footer from "@/app/components/Rodape";
-import CadastroUsuario from "./cadastro";
-import CarregarTela from "@/app/components/Loading2";
 import Tabela from "@/app/components/Tabela";
 import { FaUser} from "react-icons/fa";
-// import TabelaDark from "@/app/components/TabelaDark";
-// import TabelaDinamica from "@/app/components/TabelaDinamica";
 import CabecalhoViwer from "../../components/CabecalhoViwer";
 import Fly from "@/app/components/Flyout";
 
@@ -37,24 +33,12 @@ export default function Usuarios() {
     <>
       <Navbar/>
       <Fly/>
-      
       <CabecalhoViwer 
         nomeModel={nomeModulo} 
         Icone={FaUser}
       />
-
-      {/* <div className="flex flex-col items-center justify-center h-screen">
-          <h2 className="text-2xl font-bold mb-4">Cadastro de Usuários</h2>
-          <CadastroUsuario />
-      </div> */}
-
       <div className="">
-      {/* <div className="flex flex-col items-center justify-center h-screen"> */}
-        {/* <h2 className="text-2xl font-bold mb-4">Listagem de Usuários</h2> */}
-        {/* <TabelaDark data={usuarios}/><br /> */}
-        {/* <TabelaDinamica initialUsers={usuarios} /> */}
         <Tabela data={usuarios}/>
-        {/* <CarregarTela/> */}
       </div>
       <Footer/>
     </>

1caa55474dcdacca3d12bd56f16f55f24f603069
Romario Dias Paz <romariodp07@gmail.com>
2024-11-14 15:12:16 -0300

    Componentes - Ajustes na sombra e estilização do Botão

--- src/app/components/Botao.tsx
+++ src/app/components/Botao.tsx
@@ -18,7 +18,7 @@ export default function Botao({ texto, cor, tipo = 'submit', onClick }: BotaoPro
   return (
     <button
       type={tipo}
-      className={`${coresClasses[cor]} w-full text-white p-2 rounded-lg font-bold hover:bg-blue-700 transition-colors duration-300`}
+      className={`${coresClasses[cor]} px-4 py-2 w-full text-white p-2 rounded-lg font-bold hover:bg-blue-700 transition duration-300 border-white border shadow-md  shadow-slate-400`}
       onClick={onClick} // O onClick será executado se passado
     >
       {texto}

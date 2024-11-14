3559c24397e7b3bbdde19d500648c5f8043d7fab
Romario Dias Paz <romariodp07@gmail.com>
2024-11-14 15:11:30 -0300

    Componentes - Loads Novos

---
+++ src/app/components/LoadingVoltar.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useState } from 'react';
+import { IoGameController } from "react-icons/io5";
+import { useRouter } from 'next/navigation';
+import Botao from './Botao'; 
+
+export default function LoadVoltar({ }) {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const goBack = () => {
+    setLoading(true);
+      setTimeout(() => {
+        setLoading(false); 
+        router.back(); // Volta para a página anterior
+      }, 1000); // Tempo de simulação de carregamento    
+    };
+
+  return (
+    <div className="mt-4">  
+
+      <Botao
+        texto="Voltar"
+        cor="amarelo"
+        tipo="submit"
+        onClick={goBack}
+        
+      />
+    
+      {loading && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 z-50">
+          {/* Animação de borda circular */}
+          <div className="absolute h-32 w-32 border-t-4 border-purple-500 border-b-4 border-transparent rounded-full animate-spin"></div>
+          
+          {/* Ícone central com animação de escala */}
+          <IoGameController className="w-20 h-20 text-purple-500 animate-pulse transition-transform duration-300 ease-in-out" />
+
+        </div>
+      )}
+    </div>
+  );
+}

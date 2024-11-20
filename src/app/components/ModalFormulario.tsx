524d07c348529ee44224e8caeb4ca2417dd5d043
Romario Dias Paz <romariodp07@gmail.com>
2024-11-20 00:05:00 -0300

    Component - ModalFormulario - Ajustes

--- src/app/components/ModalFormulario.tsx
+++ src/app/components/ModalFormulario.tsx
@@ -21,12 +21,12 @@ export function ModalFormulario({ modalAberto, fecharModal, children, titulo, su
       {/* Contêiner do modal */}
       <div className="relative z-10 max-h-[90vh] w-full max-w-5xl overflow-y-auto bg-white p-6 md:rounded-lg shadow-lg">
         {/* Cabeçalho do Modal */}
-        <div className="border-b border-blue-200 pb-4">
+        <div className="border-b border-blue-400 pb-4">
           <h2 className="text-3xl font-semibold text-zinc-600 text-center">{titulo}</h2>
           <h4 className="text-md font-extralight text-zinc-500 text-center">{subtitulo}</h4>
           <button
             type="button"
-            className="absolute right-4 top-4 text-gray-400 transition-all hover:text-blue-400"
+            className="absolute right-4 top-4 text-red-500 transition-all hover:text-red-600"
             onClick={fecharModal}
           >
             <FaTimesCircle className="h-8 w-8" />

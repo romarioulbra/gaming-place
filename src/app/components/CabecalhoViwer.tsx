bc196454d4e8840f2935d602e6d3f35045973e1e
Romario Dias Paz <romariodp07@gmail.com>
2025-01-11 13:50:02 -0300

    Cabeçalho Viwer - Remoção do Cabeçalho

--- src/app/components/CabecalhoViwer.tsx
+++ src/app/components/CabecalhoViwer.tsx
@@ -4,7 +4,7 @@ export default function CabecalhoViwer({nomeModel,Icone,urlCadastro}) {
   const titulo = `Gerenciamento de ${nomeModel}`;
   return (
       <>
-        <h1 className="text-center mt-24 mb-2 text-2xl font-bold">Painel De Controle</h1>
+        {/* <h1 className="text-center mt-24 mb-2 text-2xl font-bold">Painel De Controle</h1> */}
         <div className="flex-auto ml-4 mr-4 mt-4 mb-4">
           <div className="p-8 border border-gray-300 shadow-lg flex justify-between items-center bg-white rounded-lg">        
             {/* Botões alinhados à esquerda */}

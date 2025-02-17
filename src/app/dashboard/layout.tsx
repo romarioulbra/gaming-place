25f7e06533df42e64abc45e0c91f32982fccb9c6
Romario Dias Paz <romariodp07@gmail.com>
2025-02-17 11:04:44 -0300

    Ajuste de autenticação no arquivo layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,11 @@
 import NavbarPerfil from "../components/NavBarPerfil";
-// import { SessionProvider } from "next-auth/react";
 
 export default function DashboardLayout({ children}) {
   return (
     <div>
       <NavbarPerfil/>
       <main className="">
-        {/* <SessionProvider > */}
           {children}
-        {/* </SessionProvider> */}
       </main> 
     </div>
   );

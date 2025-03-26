40d2c51d1a3a0204f571ddeced8d61494ad1517e
Romario Dias Paz <romariodp07@gmail.com>
2025-03-26 19:51:24 -0300

    Componente BotãoSenha - Modo usuarios

--- src/app/components/BotaoSenha.tsx
+++ src/app/components/BotaoSenha.tsx
@@ -4,9 +4,9 @@ export default function BotaoSenha({ mostrarModal }) {
   return (
     <button
       onClick={mostrarModal}  // Passando o tipo 'senha'
-      className="w-8 h-8 bg-blue-500 text-white rounded-full flex items-center justify-center"
+      className="w-10 h-10 bg-blue-500 text-white rounded-full flex items-center justify-center  hover:bg-blue-700 transition-colors border border-white shadow-md shadow-blue-500/50"
     >
-      <FaKey className="w-3 h-3" />
+      <FaKey className="w-4 h-4" />
     </button>
   );
 }

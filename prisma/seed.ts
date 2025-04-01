2911360fd4b82b2b13f9c0a8944c26618a7ed1c9
Romario Dias Paz <romariodp07@gmail.com>
2025-04-01 12:29:58 -0300

    Implementação de Script de Banco de Dados - (Emblemas,usuários e perfil)

---
+++ prisma/seed.ts
@@ -0,0 +1,57 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // Criar um emblema padrão
+  const emblemaPadrao = await prisma.emblemas.upsert({
+    where: { emblema_id: 1 },
+    update: {},
+    create: {
+      emblema_nome: 'Iniciante',
+      emblema_criterio: 'Primeiro login no sistema',
+      emblema_imagem: 'url_da_imagem_padrao.png',
+      emblemas_pontos: '10',
+      emblemas_status: 'Ativo',
+    },
+  });
+
+  // Criar um usuário administrador
+  const senhaCriptografada = await bcrypt.hash('admin123', 10);
+  const usuarioAdmin = await prisma.usuarios.upsert({
+    where: { usuario_email: 'admin@meusistema.com' },
+    update: {},
+    create: {
+      usuario_nome: 'Administrador',
+      usuario_email: 'admin@meusistema.com',
+      usuario_senha: senhaCriptografada,
+      usuario_nivel: 'admin',
+    },
+  });
+
+  // Criar um perfil padrão associado ao administrador e ao emblema
+  await prisma.perfis.upsert({
+    where: { perfil_id: 1 },
+    update: {},
+    create: {
+      perfil_imagem: 'default-avatar.png',
+      perfil_cidade: 'Cidade Padrão',
+      perfil_pontos: 0,
+      perfil_nivel: 1,
+      usuario: usuarioAdmin.usuario_id,
+      emblema: emblemaPadrao.emblema_id,
+    },
+  });
+
+  console.log('Seed executado com sucesso! 🚀');
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });

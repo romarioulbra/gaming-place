3a02d8711357102d63753155582917b9b10a5a4e
Romario Dias Paz <romariodp07@gmail.com>
2025-04-13 16:45:50 -0300

    Ajuste no Rodapé - Cadastro de Categorias

--- src/app/configuracao/jogos_categoria/cadastrar/page.tsx
+++ src/app/configuracao/jogos_categoria/cadastrar/page.tsx
@@ -93,7 +93,8 @@ export default function CadastroCatJogos() {
         />
       )}
 
-      <div className="flex-auto ml-4 mr-4 mt-4 mb-4">
+      {/* <div className="flex-auto ml-4 mr-4 mt-4 mb-4"> */}
+      <div className="flex-auto ml-4 mr-4 mt-4 mb-64">
         <div className="p-8 border border-gray-300 shadow-lg bg-white rounded-lg mr-2 ml-2"> 
           <form onSubmit={handleSubmit} className="bg-white p-8 rounded-lg mt-2 ">
             <div className="p-6">
